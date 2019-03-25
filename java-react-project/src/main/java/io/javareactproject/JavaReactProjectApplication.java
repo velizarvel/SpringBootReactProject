1825a72e0ee4e7cb22be7ebb6f9b66165eafc3f3
velizarvel <velizarvel@gmail.com>
2019-03-25 01:28:35 +0100

    first commit

---
+++ java-react-project/src/main/java/io/javareactproject/JavaReactProjectApplication.java
@@ -0,0 +1,13 @@
+package io.javareactproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactProjectApplication.class, args);
+	}
+
+}
